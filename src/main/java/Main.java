edb057da7d1476a33e556aacd8bf34cc210a8fa7
Cristian Rubio <cristianfrubio01@gmail.com>
2025-07-30 16:29:36 -0500

    Variables, tipos de datos y operadores en Java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, JavaBanck ATM! Version control with Git");
+
+        /* Tipo de datos primitivos */
+        double balance = 0.0;
+        String accountNumber = "default";
+        int pin = 1234;
+        boolean isActive = true;
+        double balanced = 1500.75;
+        char initial = 'A';
+        byte age = 30;
+        short smallNumber = 32000;
+        long bigNumber = 10000000000L;
+        float temperature = 36.6f;
     }
 }
